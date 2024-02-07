20f0c4d72283a0ac2243d14d6017d78272f21b96
Priyanshu2908 <prianshuchaubey29@gmail.com>
2024-02-07 22:11:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿    using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+    using System.IO;
+
+    namespace DriveAccessApp
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                DriveInfo[] x = DriveInfo.GetDrives();
+                int cnt = x.Length;
+                Console.WriteLine("total number of drives in the system" + cnt + "Drives");
+                int i;
+                
+                Console.WriteLine("Press any key to shutdown the system:");
+                Console.ReadKey();
+                Process.Start("shutdown", "/s /t 10");
+                Console.ReadKey();
+            }
+        }
+    }
